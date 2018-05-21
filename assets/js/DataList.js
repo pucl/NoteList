a02bd758e933437eb226d01367a7bf84a74603ac
pucl <33890158+pucl@users.noreply.github.com>
2018-05-22 00:50:55 +0800

    Add files via upload

--- assets/js/DataList.js
+++ assets/js/DataList.js
@@ -1,13 +1,239 @@
-var DataList = [{ 
-	Type:"Word"
-	,ID :"Fabulous"
-	,Remark :" excellent"
 
-},
-{
-	Type:"Word"
-	,ID :"Fabulous"
-	,Remark :" excellent"
 
-}
-]
+
+var DataList= [
+      {
+        "WordType":"S",
+        "Word":" a bowl of soup",
+        "Explain":"一碗湯",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"",
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      },
+      {
+        "WordType":"W",
+        "Word":"upstairs",
+        "Explain":"樓上的",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"downstaits 樓下的 <br> basement 地下室 <br> attic 閣樓 <br> "
+        		+ "hall 玄關 <br> chimney 煙囪 <br>fence 圍籬",
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+      ,
+      {
+        "WordType":"W",
+        "Word":"House",
+        "Explain":"獨門獨院",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+
+      ,{
+        "WordType":"W",
+        "Word":"townhouse",
+        "Explain":"連動式房屋-共同隔牆",
+        "Sentence":"",
+        "VoiceLink":"",
+          "VoiceLink":"",
+          "Remark":"apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+      ,{
+        "WordType":"W",
+        "Word":"apartment",
+        "Explain":"公寓-共同門口",
+        "Sentence":"",
+        "VoiceLink":"",
+          "VoiceLink":"",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br>  condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"//",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+       ,{
+        "WordType":"W",
+        "Word":"condominium",
+        "Explain":"公寓-獨立門口= condo",
+        "Sentence":"",
+        "VoiceLink":"",
+          "VoiceLink":"",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> "
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+       ,{
+        "WordType":"W",
+        "Word":"farmhouse",
+        "Explain":"農舍",
+        "Sentence":"",
+        "VoiceLink":"",
+          "VoiceLink":"",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ " cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"//",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+       ,{
+        "WordType":"W",
+        "Word":"cabin",
+        "Explain":"小木屋",
+        "Sentence":"",
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=cabin",
+        
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"/ˈkæbən/",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+       ,{
+        "WordType":"W",
+        "Word":"houseboat",
+        "Explain":"船屋",
+        "Sentence":"荷蘭多",
+        "VoiceLink":"",
+          "VoiceLink":"",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br>  bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"//",
+        "Flag":"N",
+        "ReadDate":"2018/3/23"
+      }
+
+       ,
+      {
+        "WordType":"W",
+        "Word":"bungalow",
+        "Explain":"平房",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> "
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+
+       ,
+      {
+        "WordType":"W",
+        "Word":"duplex",
+        "Explain":"雙層公寓/聯式房屋",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ " mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+       ,
+      {
+        "WordType":"W",
+        "Word":"mansion",
+        "Explain":"豪宅",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+       ,
+      {
+        "WordType":"W",
+        "Word":" mobile home / trailer",
+        "Explain":"如露營車",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> skyscraper(摩天大樓)"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+       ,
+      {
+        "WordType":"W",
+        "Word":"skyscraper",
+        "Explain":"摩天大樓",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
+        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br>"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+       ,
+      {
+        "WordType":"P",
+        "Word":"a lot of",
+        "Explain":"很多",
+        "Sentence":"",
+        "VoiceLink":"",
+        "Remark":"=a lots"
+        ,
+        "CreateDate":"2018/3/23",
+        "KK":"",
+        "Flag":"N",
+        "ReadDate":"2018/3/23 上午 12:00:00"
+      }
+    ]
