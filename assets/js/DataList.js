7e4aa5e24bf45b68dc3556aeb6e68d43b27a1570
pucl <33890158+pucl@users.noreply.github.com>
2018-06-08 01:20:12 +0800

    Add files via upload

--- assets/js/DataList.js
+++ assets/js/DataList.js
@@ -20,11 +20,11 @@ var DataList= [
         "Word":"upstairs",
         "Explain":"樓上的",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"downstaits 樓下的 <br> basement 地下室 <br> attic 閣樓 <br> "
-        		+ "hall 玄關 <br> chimney 煙囪 <br>fence 圍籬",
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=upstairs",
+        "Remark":"downstaits 樓下的 <br> basement 地下室 <br> attic[ˋætɪk] 閣樓 <br> "
+        		+ "hall[hɔl] 玄關 <br> chimney[ˋtʃɪmnɪ] 煙囪 <br>fence[fɛns] 圍籬",
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"ˋʌpˋstɛrz",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":"assets/css/images/PlayIcon.png"
@@ -35,8 +35,8 @@ var DataList= [
         "Word":"house",
         "Explain":"獨門獨院",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=house",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
@@ -52,9 +52,8 @@ var DataList= [
         "Word":"townhouse",
         "Explain":"連動式房屋-共同隔牆",
         "Sentence":"",
-        "VoiceLink":"",
-          "VoiceLink":"",
-          "Remark":"apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+          "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=townhouse",
+          "Remark":"apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
@@ -69,14 +68,13 @@ var DataList= [
         "Word":"apartment",
         "Explain":"公寓-共同門口",
         "Sentence":"",
-        "VoiceLink":"",
-          "VoiceLink":"",
-         "Remark":"townhouse(連動式房屋-共同隔牆) <br>  condominium(公寓-獨立門口) = condo <br>"
+          "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=apartment",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br>  condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"//",
+        "KK":"[əˋpɑrtmənt]",
         "Flag":"N",
         "ReadDate":"2018/3/23",
          "image":""
@@ -86,14 +84,13 @@ var DataList= [
         "Word":"condominium",
         "Explain":"公寓-獨立門口= condo",
         "Sentence":"",
-        "VoiceLink":"",
-          "VoiceLink":"",
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=condominium",
          "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> "
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[ˋkɑndə͵mɪnɪəm]",
         "Flag":"N",
         "ReadDate":"2018/3/23",
          "image":""
@@ -103,14 +100,13 @@ var DataList= [
         "Word":"farmhouse",
         "Explain":"農舍",
         "Sentence":"",
-        "VoiceLink":"",
-          "VoiceLink":"",
-         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+		"VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=farmhouse",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ " cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"//",
+        "KK":"[͵fɑrm͵haʊs]",
         "Flag":"N",
         "ReadDate":"2018/3/23",
          "image":""
@@ -122,7 +118,7 @@ var DataList= [
         "Sentence":"",
         "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=cabin",
         
-         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
@@ -137,14 +133,13 @@ var DataList= [
         "Word":"houseboat",
         "Explain":"船屋",
         "Sentence":"荷蘭多",
-        "VoiceLink":"",
-          "VoiceLink":"",
-         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+       "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=houseboat",
+         "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br>  bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"//",
+        "KK":"[ˋhaʊs͵bot]",
         "Flag":"N",
         "ReadDate":"2018/3/23",
          "image":""
@@ -156,13 +151,13 @@ var DataList= [
         "Word":"bungalow",
         "Explain":"平房",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=bungalow",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> "
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[ˋbʌŋgə͵lo]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
@@ -174,13 +169,13 @@ var DataList= [
         "Word":"duplex",
         "Explain":"雙層公寓/聯式房屋",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+        "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=duplex",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ " mansion(豪宅) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[ˋdjuplɛks]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
@@ -191,13 +186,13 @@ var DataList= [
         "Word":"mansion",
         "Explain":"豪宅",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+         "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=mansion",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mobile home / trailer (如露營車) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"ˋmænʃən]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
@@ -208,13 +203,13 @@ var DataList= [
         "Word":"mobile home / trailer",
         "Explain":"如露營車",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+         "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=trailer",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> skyscraper(摩天大樓)"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[ˋtrelɚ]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
@@ -225,13 +220,13 @@ var DataList= [
         "Word":"skyscraper",
         "Explain":"摩天大樓",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
+         "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=skyscraper",
+        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium[ˋkɑndə͵mɪnɪəm](公寓-獨立門口) = condo <br>"
         	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
         	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br>"
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[ˋskaɪ͵skrepɚ]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
@@ -254,15 +249,13 @@ var DataList= [
       {
         "WordType":"W",
         "Word":"fantastic",
-        "Explain":"摩天大樓",
+        "Explain":"口】極好的，了不起的",
         "Sentence":"",
-        "VoiceLink":"",
-        "Remark":"townhouse(連動式房屋-共同隔牆) <br> apartment(公寓-共同門口) <br> condominium(公寓-獨立門口) = condo <br>"
-        	+ "farmhouse(農舍) <br> cabin(小木屋) <br> houseboat(船屋) 荷蘭多 <br> bungalow(平房) <br>"
-        	+ "duplex(雙層公寓/聯式房屋) <br> mansion(豪宅) <br> mobile home / trailer (如露營車) <br>"
+          "VoiceLink":"https://tw.dictionary.search.yahoo.com/search?p=fantastic",
+        "Remark":""
         ,
         "CreateDate":"2018/3/23",
-        "KK":"",
+        "KK":"[fænˋtæstɪk]",
         "Flag":"N",
         "ReadDate":"2018/3/23 上午 12:00:00",
          "image":""
