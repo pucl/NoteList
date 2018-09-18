327ff0f8beba471ae22040164f822ee5400f3a7b
pucl <33890158+pucl@users.noreply.github.com>
2018-09-18 22:14:14 +0800

    Add files via upload

--- assets/js/DataList.js
+++ assets/js/DataList.js
@@ -4712,7 +4712,7 @@ var DataList= [
         ,"VoiceLink":"https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/errand"
         ,"Remark":"run an errand 跑腿"
         ,"CreateDate":"2017/05/03"
-        ,"KK":""
+        ,"KK":"[ˋɛrənd]"
         ,"Flag":"N"
         ,"ReadDate":""
         , "image":""
